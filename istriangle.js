3b6d10a1b4af5e6281796b3a7272c2aa88492470
atharvabhanage02 <atharvabhanage2002@gmail.com>
2021-10-08 13:13:26 +0530

    docs upated

---
+++ istriangle.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll(".angles")
+const checkBtn = document.querySelector(".check-btn")
+const output = document.querySelector("#output")
+
+function calculateSumofAngles(angle1,angle2,angle3){
+    const sum = angle1 + angle2 + angle3;
+    return sum;
+}
+
+function calculateIsTriangle(){
+    const sumofAngles = calculateSumofAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
+    if(sumofAngles==180){
+        output.innerText = "Yesss!! It is a Triangle"
+    }else{
+        output.innerText = "Opps! It's not a triangle ...Try another values of angles"
+    }
+}
+
+checkBtn.addEventListener("click",calculateIsTriangle)
